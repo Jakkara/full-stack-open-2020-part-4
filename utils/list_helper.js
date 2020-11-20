f1a3036b1b6057fe723e79720ce9934383072598
Jali Rainio <jali.rainio@sofokus.com>
2020-11-20 17:39:37 +0200

    4.7. Install lodash

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const totalLikes = blogs => {
   const reducer = (sum, item) => {
     return sum + item.likes
@@ -13,7 +15,49 @@ const favoriteBlog = blogs => {
   return sorted[0]
 }
 
+const mostBlogs = blogs => {
+  if (blogs.length < 1) {
+    return {}
+  }
+  const blogsByAuthor = {}
+  blogs.forEach(blog => {
+    const author = blog.author
+    if (author in blogsByAuthor) {
+      blogsByAuthor[author] += 1
+    } else {
+      blogsByAuthor[author] = 1
+    }
+  })
+  const maxKey = _.maxBy(_.keys(blogsByAuthor), key => blogsByAuthor[key])
+  return {
+    author: maxKey,
+    blogs: blogsByAuthor[maxKey]
+  }
+}
+
+const mostLikes = blogs => {
+  if (blogs.length < 1) {
+    return {}
+  }
+  const likesByAuthor = {}
+  blogs.forEach(blog => {
+    const author = blog.author
+    if (author in likesByAuthor) {
+      likesByAuthor[author] += blog.likes
+    } else {
+      likesByAuthor[author] = blog.likes
+    }
+  })
+  const maxKey = _.maxBy(_.keys(likesByAuthor), key => likesByAuthor[key])
+  return {
+    author: maxKey,
+    likes: likesByAuthor[maxKey]
+  }
+}
+
 module.exports = {
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
