14bf86ce95cd4e128df5eced2958a9cef4c64fb1
Jali Rainio <jali.rainio@sofokus.com>
2020-11-20 16:57:05 +0200

    4.4

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,8 +1,37 @@
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns 1', () => {
-  const blogs = []
+describe('Total likes', () => {
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+  const firstBook = {
+    _id: 'abcdef123567',
+    title: 'Harry Potter',
+    author: 'JK Rowling',
+    url: 'https://google.com',
+    likes: 10,
+    __v: 0
+  }
+
+  const otherBook = {
+    _id: '7654321bcdef',
+    title: 'Harry Potter 2: Here I go again',
+    author: 'JK Rowling',
+    url: 'https://google.com',
+    likes: 40,
+    __v: 0
+  }
+
+  test('List of one blog returns the count of its likes', () => {
+    const result = listHelper.totalLikes([firstBook])
+    expect(result).toBe(10)
+  })
+
+  test('Empty list has 0 likes', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+
+  test('Multiple items on list returns total sum of likes', () => {
+    const result = listHelper.totalLikes([firstBook, otherBook])
+    expect(result).toBe(50)
+  })
 })
