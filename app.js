a4326ed359f69a350c73382570f25e0029f75d00
Jali Rainio <jali.rainio@sofokus.com>
2020-12-18 16:13:14 +0200

    4.18 & 4.19

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const cors = require('cors')
 const morgan = require('morgan')
 const blogRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 app.use(cors())
 app.use(express.json())
 app.use(morgan('combined'))
 app.use('/api/blogs', blogRouter)
-app.use('/api/users/', userRouter)
+app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
